bb1a5427132d368234340920ed73e202ee1c67d8
mark.rittman <mark.rittman@rittmananalytics.com>
2023-06-14 20:49:45 +0000

    first commit

---
+++ cube.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semanticLayerSync: () => {
+    return [{
+      type: "preset",
+      name: "Preset Sync",
+      config: {
+        database: "Cube Cloud: ra-development3",
+        api_token: "44a205c2-e632-44c3-b949-9bd3dee0c18f",
+        api_secret: "ac931e30b4021510c215ab3a002b329cc8c8e1b834e223d87e31da44899706d8",
+        workspace_url: "2db0ac56.us2a.app.preset.io"
+      }
+    }];
+  }
+};
